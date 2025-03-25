41bd49cba3f197c20fdc835810c67e4ca0dc8a2b
taccione <diego.renesto@iisviolamarchesini.edu.it>
2025-03-25 20:06:22 +0100

    home page

---
+++ home/script.js
@@ -0,0 +1,10 @@
+particlesJS("particles-js", {
+    "particles": {
+        "number": {"value": 100},
+        "size": {"value": 3},
+        "move": {"speed": 2}
+    },
+    "interactivity": {
+        "events": {"onhover": {"enable": true, "mode": "repulse"}}
+    }
+});
